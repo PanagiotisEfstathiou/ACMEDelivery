c3326c12d647fb74bdcd4045503092462d00a44c
Sergi_Sampai <cs151129@uniwa.gr>
2022-08-29 16:41:30 +0300

    Updates JPA attributes and fields

--- src/main/java/com/app/acmedelivery/domainModel/Address.java
+++ src/main/java/com/app/acmedelivery/domainModel/Address.java
@@ -2,18 +2,19 @@ package com.app.acmedelivery.domainModel;
 
 
 import lombok.*;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 @Setter
 @Getter
 @ToString(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Address extends BaseModel{
-    private String address;
-    private int floor;
+    private String streetName;
     private Long telephone;
     private int addressNumber;
     private String region;
-    private String name;
     private int floorNumber;
 }
