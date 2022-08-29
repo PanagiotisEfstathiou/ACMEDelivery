a24ddad6a8770d91b09cab5c0c5c7c864b9e53de
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-08-29 16:39:28 +0300

    adds JPA attributes to Account class in the Domain layer

--- src/main/java/com/app/acmedelivery/domainModel/Account.java
+++ src/main/java/com/app/acmedelivery/domainModel/Account.java
@@ -2,18 +2,30 @@ package com.app.acmedelivery.domainModel;
 
 import lombok.*;
 
+import javax.persistence.*;
+import java.util.List;
+
 @Setter
 @Getter
 @ToString(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class Account extends BaseModel {
+@Entity
+@Table(name = "ACCOUNT", indexes = {@Index(name = "ACCOUNT_IDX_01", columnList = "email, mobilePhone")})
+@SequenceGenerator(name = "idGenerator", sequenceName = "ACCOUNT_SEQ", initialValue = 1, allocationSize = 1)
 
+public class Account extends BaseModel {
+    @Column(length = 20, nullable = false)
     private String firstName;
+    @Column(length = 20, nullable = false)
     private String lastName;
+    @Column(length = 50, nullable = false, unique = true)
     private String email;
-    private Address address;
-    private Long mobilePhone;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Address> address;
+    @Column(length = 15, nullable = false, unique = true)
+    private Short mobilePhone;
+    @Column(length = 15, nullable = false)
     private String password;
 
 
