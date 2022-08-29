093a2e431c9f66593cefe659102fdb2dd6603742
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-08-29 16:45:45 +0300

    Updates Product class in the domain layer

--- src/main/java/com/app/acmedelivery/domainModel/Product.java
+++ src/main/java/com/app/acmedelivery/domainModel/Product.java
@@ -1,4 +1,23 @@
 package com.app.acmedelivery.domainModel;
 
-public class Product {
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.xml.catalog.Catalog;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product extends BaseModel {
+    private String productName;
+    private double price;
+
 }
