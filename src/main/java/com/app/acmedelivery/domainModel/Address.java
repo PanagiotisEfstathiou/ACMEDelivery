e0587d8bd1b65d8ca955b5957229a08534fdffc6
Sergi_Sampai <cs151129@uniwa.gr>
2022-08-30 11:13:53 +0300

    Updates JPA attributes and fields

--- src/main/java/com/app/acmedelivery/domainModel/Address.java
+++ src/main/java/com/app/acmedelivery/domainModel/Address.java
@@ -2,6 +2,9 @@ package com.app.acmedelivery.domainModel;
 
 
 import lombok.*;
+
+import javax.persistence.*;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
@@ -11,10 +14,21 @@ import javax.persistence.OneToMany;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "ADDRESS")
+@SequenceGenerator(name = "storeIdGenerator", sequenceName = "ADDRESS_SEQ", initialValue = 1, allocationSize = 1)
 public class Address extends BaseModel{
+    @Column(length=25, nullable = false)
     private String streetName;
+
+    @Column(length=15, nullable = false)
     private Long telephone;
+
+    @Column(length=3, nullable=false)
     private int addressNumber;
+
+    @Column(length=15, nullable = false)
     private String region;
+
+    @Column(length=2, nullable=true)
     private int floorNumber;
 }
