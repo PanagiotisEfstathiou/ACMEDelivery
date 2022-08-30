b8a35843823cab1d33ebd7e1be666b4ca6703cb2
Sergi_Sampai <cs151129@uniwa.gr>
2022-08-30 12:44:57 +0300

    Implements Store with JPA attributes and fields

--- src/main/java/com/app/acmedelivery/domainModel/Store.java
+++ src/main/java/com/app/acmedelivery/domainModel/Store.java
@@ -1,8 +1,42 @@
 package com.app.acmedelivery.domainModel;
 
-public class Store {
+import lombok.*;
 
-    private Product product;
-    private ProductCategory productCategory;
+import javax.persistence.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+@Setter
+@Getter
+@RequiredArgsConstructor
+@ToString(callSuper = true)
+@Builder
+@Entity
+@Table(name = "STORE")
+@SequenceGenerator(name = "storeIdGenerator", sequenceName = "STORE_SEQ", allocationSize = 1)
+public class Store extends BaseModel {
+
+    @Column(length=20, nullable=false)
+    private String storeName;
+
+    @Column(length=20, nullable = false)
+    private String storeRegion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length=15, nullable =false)
+    @ElementCollection
+    private List<ProductCategory> storeCategory;
+
+    @Transient
+    @JoinColumn(name = "catalog_id")
+    private Catalog catalog;
+
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(Catalog catalog) {
+        this.catalog = catalog;
+    }
 }
