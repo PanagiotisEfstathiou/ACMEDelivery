695ce710d66c4ac2d1bb2c564fcc64a09b6746eb
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-08-30 12:49:39 +0300

    Deletes Catalog class and adds a list of product into the Store class

--- src/main/java/com/app/acmedelivery/domainModel/Catalog.java
+++
@@ -1,9 +0,0 @@
-package com.app.acmedelivery.domainModel;
-
-import java.util.List;
-
-public class Catalog extends BaseModel{
-
-
-   private List<ProductCategory> catalog;
-}
--- src/main/java/com/app/acmedelivery/domainModel/Store.java
+++ src/main/java/com/app/acmedelivery/domainModel/Store.java
@@ -1,8 +1,35 @@
 package com.app.acmedelivery.domainModel;
 
-public class Store {
+import lombok.*;
 
-    private Product product;
-    private ProductCategory productCategory;
+import javax.persistence.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+@ToString(callSuper = true)
+@Builder
+@Entity
+@Table(name = "STORE")
+@SequenceGenerator(name = "storeIdGenerator", sequenceName = "STORE_SEQ", initialValue = 1, allocationSize = 1)
+public class Store extends BaseModel {
+
+    @Column(length=20, nullable=false)
+    private String storeName;
+
+    @Column(length=20, nullable = false)
+    private String storeRegion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length=15, nullable =false)
+    @ElementCollection
+    private List<ProductCategory> storeCategory;
+   
+    @OneToMany
+    private List<Product> catalog;
+
 
 }
