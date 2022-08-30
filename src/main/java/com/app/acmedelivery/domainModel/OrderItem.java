019779147014cb575a71d28500539cfcdc3c5dc6
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-08-30 10:13:08 +0300

    Updates OrderItem class in the domain layer

--- src/main/java/com/app/acmedelivery/domainModel/OrderItem.java
+++ src/main/java/com/app/acmedelivery/domainModel/OrderItem.java
@@ -1,15 +1,25 @@
 package com.app.acmedelivery.domainModel;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Setter
 @Getter
 @ToString(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
+@Entity
 public class OrderItem extends BaseModel{
-
+    
+    @ManyToOne
     private Product product;
+    private int quantity;
     private double price;
     private String notes;
 }
