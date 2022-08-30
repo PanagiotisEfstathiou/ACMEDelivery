d345de69776e94ebc2576deb81d7fa397938cd62
Sergi_Sampai <cs151129@uniwa.gr>
2022-08-30 12:27:46 +0300

    Adds List<Product> for catalog

--- src/main/java/com/app/acmedelivery/domainModel/Catalog.java
+++ src/main/java/com/app/acmedelivery/domainModel/Catalog.java
@@ -1,9 +1,12 @@
 package com.app.acmedelivery.domainModel;
 
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
 import java.util.List;
 
+@ToString
+@SuperBuilder
 public class Catalog extends BaseModel{
-
-
-   private List<ProductCategory> catalog;
+   private List<Product> catalog;
 }
