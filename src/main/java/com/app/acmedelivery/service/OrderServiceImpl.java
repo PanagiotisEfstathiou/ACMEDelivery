fef2deece5d12c49c0d387d1c3a495c2a89939df
Sergi_Sampai <cs151129@uniwa.gr>
2022-08-31 19:47:34 +0300

    Implements initiateOrder class

--- src/main/java/com/app/acmedelivery/service/OrderServiceImpl.java
+++ src/main/java/com/app/acmedelivery/service/OrderServiceImpl.java
@@ -1,10 +1,13 @@
 package com.app.acmedelivery.service;
 
+import com.app.acmedelivery.domainModel.Account;
 import com.app.acmedelivery.domainModel.Order;
 import com.app.acmedelivery.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +17,10 @@ public class OrderServiceImpl extends BaseServiceImpl<Order> implements OrderSer
     @Override
     public JpaRepository<Order, Long> getRepository() {
         return orderRepository;
+    }
+
+    @Override
+    public Order initiateOrder(Account account){
+        return Order.builder().account(account).orderItems(new ArrayList<>()).build();
     }
 }
