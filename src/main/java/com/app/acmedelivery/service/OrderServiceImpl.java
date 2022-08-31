ced1d1e333ad840517b0ab64eea5f00ae58b9be1
Sergi_Sampai <cs151129@uniwa.gr>
2022-08-31 19:54:45 +0300

    Implements addItem method

--- src/main/java/com/app/acmedelivery/service/OrderServiceImpl.java
+++ src/main/java/com/app/acmedelivery/service/OrderServiceImpl.java
@@ -2,6 +2,8 @@ package com.app.acmedelivery.service;
 
 import com.app.acmedelivery.domainModel.Account;
 import com.app.acmedelivery.domainModel.Order;
+import com.app.acmedelivery.domainModel.OrderItem;
+import com.app.acmedelivery.domainModel.Product;
 import com.app.acmedelivery.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,5 +24,29 @@ public class OrderServiceImpl extends BaseServiceImpl<Order> implements OrderSer
     @Override
     public Order initiateOrder(Account account){
         return Order.builder().account(account).orderItems(new ArrayList<>()).build();
+    }
+
+    @Override
+    public void addItem(Order order, Product product, int quantity){
+        if(checkNullability(order, product)){
+            return;
+        }
+
+        boolean increasedQuantity = false;
+
+        // If product is already contained in the order, don't add it again, just increase the quantity accordingly
+        for (OrderItem oi : order.getOrderItems()) {
+            if (oi.getProduct().getSerial().equals(product.getSerial())) {
+                oi.setQuantity(oi.getQuantity() + quantity);
+                increasedQuantity = true;
+                break;
+            }
+        }
+
+        if (!increasedQuantity) {
+            order.getOrderItems().add(newOrderItem(order, product, quantity));
+        }
+
+        logger.debug("Product[{}] added to Order[{}]", product, order);
     }
 }
