76c15546043c04db09c706cd88eb785a7393f510
Sergi_Sampai <cs151129@uniwa.gr>
2022-08-31 16:22:31 +0300

    Fixes minor things in class

--- src/main/java/com/app/acmedelivery/service/OrderServiceImpl.java
+++ src/main/java/com/app/acmedelivery/service/OrderServiceImpl.java
@@ -1,11 +1,18 @@
 package com.app.acmedelivery.service;
 
 import com.app.acmedelivery.domainModel.Order;
+import com.app.acmedelivery.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class OrderServiceImpl extends BaseServiceImpl<Order> implements OrderService{
+
+    private final OrderRepository orderRepository;
     @Override
     public JpaRepository<Order, Long> getRepository() {
-        return null;
+        return orderRepository;
     }
 }
