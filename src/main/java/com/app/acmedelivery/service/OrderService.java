5540fbb9424df87152e04d4ed6e06f255aa2325d
Sergi_Sampai <cs151129@uniwa.gr>
2022-09-01 16:45:58 +0300

    Adds methods about OrderService

--- src/main/java/com/app/acmedelivery/service/OrderService.java
+++ src/main/java/com/app/acmedelivery/service/OrderService.java
@@ -1,6 +1,25 @@
 package com.app.acmedelivery.service;
 
+import com.app.acmedelivery.domainModel.Account;
 import com.app.acmedelivery.domainModel.Order;
+import com.app.acmedelivery.domainModel.PaymentMethod;
+import com.app.acmedelivery.domainModel.Product;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
 public interface OrderService extends  BaseService<Order>{
+
+    Order initiateOrder(Account account);
+
+    void addItem(Order order, Product product, int quantity);
+
+    void updateItem(Order order, Product product, int quantity);
+
+    void removeItem(Order order, Product product);
+
+    Order checkout(Order order, PaymentMethod paymentMethod, BigDecimal orderTotalPrice);
+
+    List<Order> findBySubmitDate(Date submitDate);
 }
