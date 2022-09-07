83faf80c713de4ff5b00fae1ba7a77352f982729
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-09-07 15:24:40 +0300

    Fixes List<Product> to Set<Product> and fixes StoreCategory in Store.java

--- src/main/java/com/app/acmedelivery/domainModel/Store.java
+++ src/main/java/com/app/acmedelivery/domainModel/Store.java
@@ -1,14 +1,19 @@
 package com.app.acmedelivery.domainModel;
 
 import lombok.*;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
+
 @Setter
 @Getter
 @RequiredArgsConstructor
 @Entity
 @Table(name = "STORES")
+@Builder
 @SequenceGenerator(name = "storeIdGenerator", sequenceName = "STORE_SEQ", allocationSize = 1)
 public class Store extends BaseModel {
 
@@ -21,10 +26,12 @@ public class Store extends BaseModel {
     @Enumerated(EnumType.STRING)
     @Column(length=15, nullable =false)
     @ElementCollection
-    private List<ProductCategory> storeCategory;
-   
-    @OneToMany
-    private List<Product> catalog;
+    private List<StoreCategory> storeCategory;
+
+
+    @OneToMany(mappedBy = "store")
+    private Set<Product> catalog;
+
 
     @Enumerated(EnumType.STRING)
     @Column(length=15, nullable =false)
