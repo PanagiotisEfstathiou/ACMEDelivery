b2729650fdc4b2f13f619e40f2b16edf55fca6e2
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-09-14 12:41:08 +0300

    Fixes Annotation on Application class

--- src/main/java/com/app/acmedelivery/AcmeDeliveryApplication.java
+++ src/main/java/com/app/acmedelivery/AcmeDeliveryApplication.java
@@ -1,10 +1,17 @@
 package com.app.acmedelivery;
 
+import com.app.acmedelivery.base.BaseComponent;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+
 
 @SpringBootApplication
-public class AcmeDeliveryApplication {
+@EnableJpaRepositories
+public class AcmeDeliveryApplication extends BaseComponent {
 
     public static void main(String[] args) {
         SpringApplication.run(AcmeDeliveryApplication.class, args);
