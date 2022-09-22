59e39d67603a81b85758f4ea7940b503bd46b4e2
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-09-22 16:03:20 +0300

    Adds a sequence generator in orderItem and fixes some fields to display them to a json file

--- src/main/java/com/app/acmedelivery/domainModel/OrderItem.java
+++ src/main/java/com/app/acmedelivery/domainModel/OrderItem.java
@@ -1,9 +1,23 @@
 package com.app.acmedelivery.domainModel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.sun.istack.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
 @Setter
@@ -14,21 +28,26 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Entity
 @Table(name = "ORDER_ITEMS")
+@SequenceGenerator(name = "idGenerator", sequenceName = "ORDERITEM_SEQ", allocationSize = 1)
 public class OrderItem extends BaseModel{
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @NotNull
+
+    @ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnore
     private Order order;
 
-    @OneToOne(fetch = FetchType.LAZY, optional = false)
-    @NotNull
-    private Product product;
+	@JoinColumn
+	@OneToOne(fetch = FetchType.EAGER)
+    @JsonInclude
+	private Product product;
 
     @Column(nullable = false)
     @NotNull
+	@JsonInclude
     private Integer quantity;
 
-    @Column(precision = 10, scale = 2, nullable = false)
+    @Column
     @NotNull
+	@JsonInclude
     private BigDecimal price;
 }
