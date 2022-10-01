c80281d550e8230a89230e3a2b0546d7438ae8f5
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-10-01 11:14:28 +0300

    Adds queries to search store by name and by category and get catalog in Store repository

--- src/main/java/com/app/acmedelivery/repository/StoreRepository.java
+++ src/main/java/com/app/acmedelivery/repository/StoreRepository.java
@@ -1,38 +1,34 @@
 package com.app.acmedelivery.repository;
 
 import com.app.acmedelivery.domainModel.Store;
+import com.app.acmedelivery.domainModel.StoreCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import javax.persistence.QueryHint;
+import java.util.Optional;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
 
 
-    Store findByStoreName(String StringName) ;
+	@Query("select o from Store o where o.storeName = :storeName")
+	@QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+	Optional<Store> findByStoreName(String storeName);
+
+	@Query("select o from Store o where o.storeCategory = :storeCategory")
+	@QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+	Optional<Store> findByStoreCategory(StoreCategory storeCategory);
+
+	@Query("select o from Store o join fetch o.catalog where o.id = :id")
+	@QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+	Optional<Store> getLazy(Long id);
 
 
 
 
-    @Query(
-            value = "select distinct(rating.store) ,avg(rating.value)\n" +
-                    "from rating\n" +
-                    "group by (rating.store)\n" +
-                    "order by avg(rating.value) DESC",
-            nativeQuery = true)
-    List<Store>mostFamousStores();
-    @Query(
-            value ="select stores.name, avg(rating.value)\n"+
-                    "from rating\n"+
-                    "inner join stores\n"+
-                    "on stores.storeid = rating.store\n"+
-                    "where stores.category LIKE 'AAAA'\n"+
-                    "group by stores.name\n",
-            nativeQuery = true)
-
-    List<Store>mostFamousStoresPerCategory();
 
 
 
