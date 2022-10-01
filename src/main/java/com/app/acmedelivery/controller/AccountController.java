97007ee9517173eaf5e1481e90004035f2a18672
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-10-01 10:30:44 +0300

    Adds a find all response in the account controller

--- src/main/java/com/app/acmedelivery/controller/AccountController.java
+++ src/main/java/com/app/acmedelivery/controller/AccountController.java
@@ -1,17 +1,21 @@
 package com.app.acmedelivery.controller;
 
 import com.app.acmedelivery.domainModel.Account;
+import com.app.acmedelivery.domainModel.Store;
+import com.app.acmedelivery.repository.AccountRepository;
 import com.app.acmedelivery.service.AccountService;
 import com.app.acmedelivery.service.BaseService;
 import com.app.acmedelivery.transfer.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Email;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -20,13 +24,20 @@ import java.util.NoSuchElementException;
 public class AccountController extends AbstractController<Account>{
 
     private final AccountService accountService;
+	private final AccountRepository accountRepository;
 
     @Override
     protected BaseService<Account> getBaseService() {
         return accountService;
     }
 
-    @GetMapping(params = "email")
+	@CrossOrigin(origins = "*")
+	@GetMapping("allAccounts")
+	public List<Account> getAccounts(){
+		return this.accountRepository.findAll();
+	}
+
+    @RequestMapping(params = "email")
     public ResponseEntity<ApiResponse<Account>> findByEmail(@Email @RequestParam String email) {
         final Account byEmail = accountService.findByEmail(email);
         if (byEmail == null) {
