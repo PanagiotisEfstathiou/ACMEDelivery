e54723a475cef0fdf5e5ae43dfe9d15c30216519
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-10-01 10:33:29 +0300

    Adds a find all response in the order controller

--- src/main/java/com/app/acmedelivery/controller/OrderController.java
+++ src/main/java/com/app/acmedelivery/controller/OrderController.java
@@ -4,8 +4,12 @@ import com.app.acmedelivery.domainModel.Order;
 import com.app.acmedelivery.service.BaseService;
 import com.app.acmedelivery.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +21,11 @@ public class OrderController extends AbstractController<Order> {
 	@Override
 	public BaseService<Order> getBaseService() {
 		return orderService;
+	}
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/getAll")
+	public List<Order> getAllOrders(){
+		return orderService.findAll();
 	}
 }
