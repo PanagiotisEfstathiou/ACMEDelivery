573cd7751b50678f4f22ee6d5dc22b33c6615153
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-10-01 10:35:08 +0300

    Adds a @JsonBackReference to store in the Product class

--- src/main/java/com/app/acmedelivery/domainModel/Product.java
+++ src/main/java/com/app/acmedelivery/domainModel/Product.java
@@ -1,10 +1,20 @@
 package com.app.acmedelivery.domainModel;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
@@ -23,23 +33,27 @@ public class Product extends BaseModel {
     @NotNull
     private String name;
 
-    @Column(precision = 10, scale = 2, nullable = false)
+    @Column
     @NotNull
     private BigDecimal price;
 
     @Column(length = 150, nullable = false)
     private String description;
 
+
+	@JsonBackReference("store")
     @NotNull
-    @ManyToOne
-	@JsonIgnore
+    @ManyToOne()
     private Store store;
+
+
 
 
     @Column(length = 50, nullable = false)
     @NotNull
     @Enumerated(EnumType.STRING)
     private ProductCategory productCategory;
+
 
 
 
